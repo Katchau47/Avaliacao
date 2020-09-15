7282d7db682b576b50cb912cd58e74b0ac19523f
Katchau47 <68088093+Katchau47@users.noreply.github.com>
2020-09-15 18:33:25 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,33 +1,30 @@
 #include "help.h"
 
-#define NULL 0
-
 int main(){
-char opc, cpf[14];
+setlocale(LC_ALL,"PORTUGUESE");
+char opc;
+char cpf[14];
 cliente c;
-char b;
 opc=menu();
 do{
     switch (opc)
     {
     case '1':
-        printf("\n\ndigite o cpf do cliente\n\n");
-        scanf("%s",&cpf);
-        b=busca_cliente(cpf);
-        if(b==NULL)
-            printf("\n\nCliente naum encontrado\n\n");
-        else
-            printf("\n\nCliente encontrado\n\n");
+        system("cls");
+        fflush(stdin);
+        printf("// ----- // ----- // BUSCA HÓSPEDE // ----- // ----- // \n\n");
+        printf("\nDigite o cpf que será buscado: ");
+        gets(cpf);
+        busca_cliente(&cpf);
     break;
 
     case '2':
-        printf("\nChamar funcao cadastro...\n\n");
         cadastra_cliente (c);
 
     break;
 
     case '3':
-        printf("\nChamar funcao att...\n\n");
+        printf("\nChamar função att...\n\n");
 
     break;
 
@@ -37,20 +34,18 @@ do{
     break;
 
     case '5':
-        printf("\nChamar funcao imprime...\n\n");
-
+        imprime_cliente(c);
     break;
 
     case '6':
-        printf("\nChamar funcao lista...\n\n");
-
+        printf("\nChamar funcao relatório...\n\n");
     break;
 
     case '9':
     break;
 
     default:
-    printf("\nOpcao invalida!\n\n");
+    printf("\nOpção inválida!\n\n");
     break;
   }
   opc=menu();
@@ -61,82 +56,105 @@ return 0;
 
 char menu (){
 char op;
-printf("// ----- // ----- // CADASTRO HOSPEDE // ----- // ----- // \n\n[1] - Buscar por cadastro \n[2] - Cadastrar novo\n[3] - Atualizar cadastro\n[4] - Remover cadastro\n[5] - Imprimir todos cadastrados\n[6] - Gerar relatorio\n[9] - Finalizar \n-------------------------------\nEntre com a sua opcao: ");
+printf("\n\n// ----- // ----- // CADASTRO HÓSPEDE // ----- // ----- // \n\n[1] - Buscar por cadastro \n[2] - Cadastrar novo\n[3] - Atualizar cadastro\n[4] - Remover cadastro\n[5] - Imprimir todos cadastrados\n[6] - Gerar relatório\n[9] - Finalizar \n-------------------------------\nEntre com a sua opção: ");
 scanf("\n%c",&op);
 return (op);
 }
 
-char busca_cliente(char cpf){
- cliente comp;
- int i;
-if ((hos = fopen("hospede.dat", "r+")) == NULL)
-    printf("Nao foi possivel abrir o arquivo!\n");
-else {
-    while (!feof(hos))
-    {
-        for (i=0;i < 200; i++)
-        {
-            fscanf(hos, "%s ", &comp.cpf);
-            if (cpf==comp.cpf)
-            {
-                printf("cliente encontrado");
-                return (cpf);
-            }
 
-            else
-            {
-                return NULL;
-            }
-
-
-        };
-    };
-};
-fclose(hos);
-}
-
-void cadastra_cliente(cliente c){
-printf("Cpf ");
-scanf("%s", c.cpf);
-int val=busca_cliente(c.cpf);
-if (val==0)
-{
-    printf("Nome: ");
-    scanf("%s", c.nome);
-    printf("Sobrenome: ");
-    scanf("%s", c.sobrenome);
-    printf("Telefone: ");
-    scanf("%s", c.telefone);
-    printf("Sexo: ");
-    scanf("%s", c.sexo);
-    printf("Idade: ");
-    scanf("%d", c.idade);
-}
-hos = fopen("hospede.dat", "w");
+char busca_cliente(char *cpf){
+hos = fopen("hospede.dat", "r");
+cliente c;
 if (hos == NULL) {
-puts("Nao foi possivel abrir o arquivo!\n");
-exit(1);
+    puts("Nao foi possivel abrir o arquivo!\n");
+    exit(1);
 }
-fprintf(hos, "%d ", c); // escreve o numero no arquivo
- fclose(hos);
+else
+{
+    while(fread(&c, sizeof(cliente), 1, hos)==1){
+        if(strcmp(cpf, c.cpf)==0)
+        {
+            printf("\n// ----- // ----- //// ----- // ----- // \n");
+            printf("\n\nCadastro existente encontrado\n\n");
+            printf("\n// ----- // ----- //// ----- // ----- // \n");
+            system("pause");
+            system("cls");
+            return cpf;
+        }
+    else
+    {
+        printf("\n// ----- // ----- //// ----- // ----- // \n");
+        printf("\n\nCliente ainda não cadastrado\n\n");
+        printf("\n// ----- // ----- //// ----- // ----- // \n");
+        system("pause");
+        system("cls");
+        return NULL;
+    };
+
+    };
+    fclose(hos);
+}
 }
 
+void cadastra_cliente(){
+system("cls");
+printf("// ----- // ----- // CADASTRO HÓSPEDE // ----- // ----- // \n\n");
+printf("Digite o CPF:");
+cliente c;
+scanf("%s", c.cpf);
+hos = fopen("hospede.dat", "ab");
+int val;
+if (hos == NULL) {
+    puts("Nao foi possivel abrir o arquivo!\n");
+    exit(1);
+}
+else{
+    val=busca_cliente(c.cpf);
+    if (val==NULL)
+        {
+        fflush(stdin);
+        printf("Nome: ");
+        gets(c.nome);
+        printf("Sobrenome: ");
+        gets(c.sobrenome);
+        printf("Telefone: ");
+        gets(c.telefone);
+        printf("Sexo: ");
+        scanf("%c", &c.sexo);
+        printf("Idade:");
+        scanf("%d", &c.idade);
+        fwrite(&c,sizeof (cliente),1, hos);
+        fclose(hos);
+        }
+        }
+system("cls");
+}
 
-//int valida_cpf(char cpf, char ccpf){
-   // int i;
-   // for (i = 0; str1[i] != '\0' && str2[i] != '\0'; i++){
-        //assim que uma letra seja diferente
-       // if (str1[i] != str2[i]){
-            //retorna -1 se a primeira for menor ou 1 caso contrário
-           // return str1[i] < str2[i] ? -1 : 1;
-       // }
-    //}
-    //Se as duas acabaram, são iguais e retorna 0. Se a str1 acabou e str2 não
-    //retorna um valor negativo. Se str2 acabou e a str1 retorna um valor positivo
-    //return str1[i] - str2[i];
-//}
-
-
+void imprime_cliente(cliente c){
+system("cls");
+hos = fopen("hospede.dat", "rb");
+if (hos == NULL) {
+    puts("Nao foi possivel abrir o arquivo!\n");
+    exit(1);
+}
+else
+{
+    printf("\n// ----- // ----- // LISTA DE CADASTROS // ----- // ----- // \n");
+    while(fread(&c, sizeof(cliente), 1, hos)==1)
+    {
+        printf("\nCPF: %s \n", c.cpf);
+        printf("Nome: %s \n", c.nome);
+        printf("Sobrenome: %s \n", c.sobrenome);
+        printf("Telefone: %s \n", c.telefone);
+        printf("Sexo: %c \n", c.sexo);
+        printf("Idade: %d ", c.idade);
+        printf("\n// ----- // ----- //// ----- // ----- // \n");
+    }
+    fclose(hos);
+}
+system("pause");
+system("cls");
+}
 
 
 
